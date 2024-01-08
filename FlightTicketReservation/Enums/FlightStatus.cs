1bd25d7e47bd74a2d5ba6159dadee4ef023eef6c
altaniket <ialtantekin@gmail.com>
2024-01-08 11:35:26 +0300

    Add project files.

---
+++ FlightTicketReservation/Enums/FlightStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightTicketReservation.Enums
+{
+    internal enum FlightStatus
+    {
+        LastCall,
+        Departed,
+        GateClosed,
+        Boarding,
+        CounterOpen,
+        CounterClosed,
+        Delayed
+    }
+}
